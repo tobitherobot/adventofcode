952d18286ab3de126602aa70f41d944dfc06efa1
tobitherobot <baumann.tobias@outlook.com>
2020-10-22 09:27:24 +0200

    solved day 1 y19

---
+++ src/main/java/adventofcode/y19/D01.java
@@ -0,0 +1,45 @@
+package adventofcode.y19;
+
+import java.util.List;
+
+import adventofcode.Day;
+import adventofcode.FileReader;
+
+/**
+ * 
+ * @author tba
+ *
+ */
+public class D01 implements Day
+{
+	public Object star1() 
+	{
+		List<Integer> modules = new FileReader(1,19).getIntList();
+		long sum = 0L;
+		
+		for (int module : modules) {
+			sum += Math.max(0, module/3-2);
+		}
+		return sum;
+	}
+
+	public Object star2() 
+	{
+		List<Integer> modules = new FileReader(1,19).getIntList();
+		long sum = 0L;
+		
+		for (int module : modules) {
+			sum += getFuel(module);
+		}
+		return sum;
+	}
+	
+	private long getFuel(long m)
+	{
+		long fuel = Math.max(0, m/3-2);
+		
+		if (0<fuel) fuel += getFuel(fuel);
+		
+		return fuel;
+	}
+}
