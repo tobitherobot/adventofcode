bdb22534ae190181ed80100deb5a0e5b6001c0a1
tobitherobot <baumann.tobias@outlook.com>
2020-12-12 18:37:22 +0100

    added day template

---
+++ src/main/java/adventofcode/D00.java
@@ -0,0 +1,23 @@
+package adventofcode;
+
+import adventofcode.Day;
+
+/**
+ * template day class
+ */
+public class D00 implements Day
+{
+	public Object star1() 
+	{
+		
+		
+		return null;
+	}
+
+	public Object star2() 
+	{
+		
+		
+		return null;
+	}
+}
