a9f40a43e70281129a3e90db3ba10862d6ca982a
tobitherobot <baumann.tobias@outlook.com>
2021-11-13 18:28:12 +0100

    solved 20-05

---
+++ y20/d05.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+p = Path(__file__).with_name("r05.txt")
+with p.open("r") as f:
+    input = [x.strip() for x in f.readlines()]    
+
+# star 1
+mx = 0
+for passport in input:
+    row = 0
+    seat = 0
+    for i in range(7):
+        row <<= 1
+        if passport[i]=='B':
+            row += 1
+    for i in range(7,10):
+        seat <<= 1
+        if passport[i]=='R':
+            seat += 1
+    mx = max(mx, row*8 + seat)
+print(mx)
+
+ids = []
+for passport in input:
+    row = 0
+    seat = 0
+    for i in range(7):
+        row <<= 1
+        if passport[i]=='B':
+            row += 1
+    for i in range(7,10):
+        seat <<= 1
+        if passport[i]=='R':
+            seat += 1
+    ids.append(row*8 + seat)
+ids.sort()
+for i in range(len(ids)-1):
+    if ids[i+1] != ids[i]+1:
+        print(ids[i]+1)
+        break
