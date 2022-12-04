b3b1af4976a1239b9b056d6133705aaf2cb174ef
tobitherobot <baumann.tobias@outlook.com>
2022-12-04 12:48:09 +0100

    updated y21 d18

--- y21/d18.py
+++ y21/d18.py
@@ -1,6 +1,6 @@
 
 with open("y21/r18.txt") as f:
-    input = [l.strip() for l in f.readlines()]
+    inp = [l.strip() for l in f.readlines()]
 
 class Pair: # messy af
     def parse(self, input):
@@ -22,6 +22,7 @@ class Pair: # messy af
 
     def update(self, depth):
         self.set_depth(depth) # depth = depth
+        # print(p)
         flag = False
         
         if isinstance(self.first, Pair):
@@ -29,24 +30,24 @@ class Pair: # messy af
         if isinstance(self.second, Pair):
             flag = flag or self.second.update(depth+1)
 
-        if 4 <= self.depth and isinstance(self.first, int) and isinstance(self.second, int):
+        if not flag and 4 <= self.depth and isinstance(self.first, int) and isinstance(self.second, int):
             self.explode()
             flag = True
-        elif isinstance(self.first, int) and 9 < self.first:
+        elif not flag and isinstance(self.first, int) and 9 < self.first:
             pair = Pair("[0,0]", 0, self.depth+1)
-            pair.first = self.first // 2
-            pair.second = self.first // 2 + 1
+            pair.first = (self.first // 2)
+            pair.second = (self.first // 2 + self.first % 2)
             pair.previous = self
             self.first = pair
             flag = True
-        if isinstance(self.second, int) and 9 < self.second:
+        elif not flag and isinstance(self.second, int) and 9 < self.second:
             pair = Pair("[0,0]", 0, self.depth+1)
-            pair.first = self.second // 2
-            pair.second = self.second // 2 + 1
+            pair.first = (self.second // 2)
+            pair.second = (self.second // 2 + self.second % 2)
             pair.previous = self
             self.second = pair
             flag = True
-        
+        # print(p)
         return flag
 
     def explode(self):
@@ -57,29 +58,29 @@ class Pair: # messy af
             self.second.explode()
             return True
         else:        
-            me = self
+            me1 = self
             value = self.first
-            while me.previous != None:
-                if me.previous.second == me: # add first value
-                    if isinstance(me.previous.first, Pair):
-                        me.previous.first.add_second(value)
+            while me1.previous != None:
+                if me1.previous.second == me1: # add first value
+                    if isinstance(me1.previous.first, Pair):
+                        me1.previous.first.add_second(value)
                     else:
-                        me.previous.first += value
+                        me1.previous.first += value
                     break
                 else:
-                    me = me.previous
+                    me1 = me1.previous
 
-            me = self
+            me2 = self
             value = self.second
-            while me.previous != None:
-                if me.previous.first == me: # add second value
-                    if isinstance(me.previous.second, Pair):
-                        me.previous.second.add_first(value)
+            while me2.previous != None:
+                if me2.previous.first == me2: # add second value
+                    if isinstance(me2.previous.second, Pair):
+                        me2.previous.second.add_first(value)
                     else:
-                        me.previous.second += value
+                        me2.previous.second += value
                     break
                 else:
-                    me = me.previous
+                    me2 = me2.previous
             
             if self.previous.first == self:
                 self.previous.first = 0
@@ -129,9 +130,13 @@ class Pair: # messy af
         return '[' + str(self.first) + "," + str(self.second) + "]"#(" + str(self.depth) + ")"
 
 # star 1
-p = Pair(input[0], 0, 0)
+global p
+p = Pair(inp[0], 0, 0)
 print(p)
-for i in range(1, len(input)):
-    p = p.add(Pair(input[i], 0, 0))
+for i in range(1, len(inp)):
+    p = p.add(Pair(inp[i], 0, 0))
     flag = True
-    # while flag:
+    while flag:
+        flag = p.update(0)
+        print(p)
+        # input("")
