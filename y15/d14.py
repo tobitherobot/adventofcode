fbcc984f937c49d07b85b72f5179117291bd28bb
tobitherobot <baumann.tobias@outlook.com>
2022-12-07 19:04:56 +0100

    solved y15 d14 star 1

---
+++ y15/d14.py
@@ -0,0 +1,18 @@
+with open('y15/r14.txt') as f:
+    inp = []
+    for x in f.readlines():
+        l = x.strip().split(' ')
+        inp.append([int(l[3]), int(l[6]), int(l[13])]) # kms sec rest
+
+# star 1
+sec = 2503
+mx = 0
+for rein in inp:
+    dist = (sec // (rein[1]+rein[2])) * rein[1] * rein[0]
+    rest = sec % (rein[1]+rein[2])
+    if rein[1] <= rest:
+        dist += rein[1]*rein[0]
+    else:
+        dist += rest*rein[0]
+    mx = max(mx, dist)
+print(mx)
